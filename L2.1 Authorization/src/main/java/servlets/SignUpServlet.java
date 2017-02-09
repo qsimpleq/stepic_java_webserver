dbe93cdae5452e81ce59e41f06307f6959dcb6d0
Kirill Babikhin <mrakobes86reg@yandex.ru>
2017-02-09 17:43:36 +0500

    Добавил SignUpServlet

---
+++ L2.1 Authorization/src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignUpServlet extends HttpServlet {
+    private AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Unauthorized");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile == null) {
+            accountService.addNewUser(new UserProfile(login, pass, ""));
+        }
+
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("Authorized: " + login);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
